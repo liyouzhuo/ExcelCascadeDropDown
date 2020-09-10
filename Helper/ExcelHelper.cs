3ea252b5bf2a5998e3f26c8f750d881a42e84cb7
liyouzhuo <1593500177@qq.com>
2020-09-10 10:21:35 +0800

    初次提交

---
+++ Helper/ExcelHelper.cs
@@ -0,0 +1,22 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public class ExcelHelper
+    {
+        public void ExportExcel()
+        {
+            // 创建excel工作簿
+            IWorkbook workbook = new XSSFWorkbook();
+            // 创建单元格样式
+            ICellStyle cellStyle = workbook.CreateCellStyle();
+
+        }
+    }
+}
